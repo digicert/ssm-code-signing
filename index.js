8393b6e306dc09e15f8849ad0a0ff30daf90e6b6
Yashdigi <108860263+Yashdigi@users.noreply.github.com>
2022-11-08 11:20:05 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import { main } from "@digicert/ssm-client-tools-installer";
-import * as core from "@actions/core";
-
-try {
-  const result = await main("keypair-signing");
-  core.setOutput("Result", result);
-} catch (error) {
-  core.setFailed(error.message);
-}
