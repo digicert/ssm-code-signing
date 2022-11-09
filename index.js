28fc76bbd82af3466a23a53fb09918c443df50f0
Yashdigi <108860263+Yashdigi@users.noreply.github.com>
2022-11-09 19:21:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import  {main} from "@digicert/ssm-client-tools-installer";
 import * as core from "@actions/core"
 
 const result=await main("keypair-signing")
-core.setoutput("result",result)
+core.setOutput("result",result)
