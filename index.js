34e18bc142f69630af30e1cf1cecf09b7b11bfad
Yashdigi <108860263+Yashdigi@users.noreply.github.com>
2022-11-23 16:02:10 +0530

    new changes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import * as core from "@actions/core";
 
 try {
   const result = await main("keypair-signing");
-
-  !result
-    ? core.setOutput("Result", "Success")
-    : core.setFailed("Installation failed");
+  const message = JSON.parse(result);
+  if (message) {
+    core.setOutput("extractPath", message.imp_file_paths.extractPath);
+    core.setOutput("PKCS11_CONFIG", message.imp_file_paths.PKCS11_CONFIG);
+  } else {
+    core.setFailed("Installation Failed");
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
