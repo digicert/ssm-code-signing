5166a95e19c3c977bdc2b0bba5cb8e6ae769d7f4
Yashdigi <108860263+Yashdigi@users.noreply.github.com>
2022-11-25 18:58:17 +0530

    sign

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { main } from "@digicert/ssm-client-tools-installer";
 import * as core from "@actions/core";
 import * as tc from "@actions/tool-cache"
+import * as tl from "azure-pipelines-task-lib/task";
+import path from 'path'
+import *as semver from 'semver'
 
 try {
 const resolvedVersion="1.31.0"
@@ -26,15 +29,33 @@ const resolvedVersion="1.31.0"
     .catch((err:any) => {
      throw err;
     });
-    findToolInPath()
+  const signtoolpath=  findToolInPath("C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\","signtool")
+ console.log("**",signtoolpath)
 } catch (error:any) {
   core.setFailed(error.message);
 }
 
-function findToolInPath() {
-  const toolsList = tc.find(
-    "signtool.exe",
-    "x.x.x"
+function findToolInPath(pathForTool: string, tool: string) {
+  const toolsList = tl.findMatch(
+    tl.resolve(pathForTool),
+    [`${tool}*`, "!*.jar"],
+    undefined,
+    { matchBase: true }
   );
-  console.log("***",toolsList)
+  if (!toolsList || toolsList.length === 0) {
+    throw new Error(`Could not find ${tool} path`);
   }
+  //this is to find the latest SDK as there will be multiple installed versions
+  toolsList.sort((a: string, b: string) => {
+    const toolBaseDirA = path.basename(path.dirname(a));
+    const toolBaseDirB = path.basename(path.dirname(b));
+    if (semver.valid(toolBaseDirA) && semver.valid(toolBaseDirB)) {
+      return semver.rcompare(toolBaseDirA, toolBaseDirB);
+    } else if (semver.valid(toolBaseDirA)) {
+      return -1;
+    } else {
+      return toolBaseDirA.localeCompare(toolBaseDirB);
+    }
+  });
+  return toolsList[0];
+}
