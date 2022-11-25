44e4f9fcd4c895e0bb011c4c1cdd94c65e2a659d
Yashdigi <108860263+Yashdigi@users.noreply.github.com>
2022-11-25 19:50:18 +0530

    git

--- index.ts
+++ index.ts
@@ -1,54 +1,58 @@
 import { main } from "@digicert/ssm-client-tools-installer";
 import * as core from "@actions/core";
-import * as tc from "@actions/tool-cache"
+import * as tc from "@actions/tool-cache";
 import * as tl from "azure-pipelines-task-lib/task";
-import path from 'path'
-import *as semver from 'semver'
-import  * as globber from "@actions/glob"
+import path from "path";
+import * as semver from "semver";
+import * as globber from "@actions/glob";
 try {
-const resolvedVersion="1.31.0"
- process.env.SHOULD_CHECK_INSTALLED="false"
+  const resolvedVersion = "1.31.0";
+  process.env.SHOULD_CHECK_INSTALLED = "false";
   main("keypair-signing")
-    .then((result:any) => {
+    .then((result: any) => {
       const message = JSON.parse(result);
       if (message) {
         core.setOutput("extractPath", message.imp_file_paths.extractPath);
         core.addPath(message.imp_file_paths.extractPath);
-         tc.cacheDir(
+        tc.cacheDir(
           message.imp_file_paths.extractPath,
-          'smctl',
+          "smctl",
           resolvedVersion
-        ).then((response)=>{
-          console.log("tools cache has been updated with the path:",response)
+        ).then((response) => {
+          console.log("tools cache has been updated with the path:", response);
         });
         core.setOutput("PKCS11_CONFIG", message.imp_file_paths.PKCS11_CONFIG);
       } else {
         core.setFailed("Installation Failed");
       }
     })
-    .catch((err:any) => {
-     throw err;
+    .catch((err: any) => {
+      throw err;
     });
-  const foundfile= findToolInPath("C:\\Program Files (x86)\\Windows Kits\\10\\bin\\**\\x64","signtool")
-  if(foundfile){
-    core.addPath(foundfile)
-    tc.cacheDir(foundfile,"signtool","1.1.1")
+  const foundfile = findToolInPath(
+    "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\**\\x64",
+    "signtool"
+  );
+  if (foundfile) {
+    core.addPath(foundfile);
+    tc.cacheDir(foundfile, "signtool", "1.1.1");
   }
-} catch (error:any) {
+} catch (error: any) {
   core.setFailed(error.message);
 }
 
 function findToolInPath(pathForTool: string, tool: string) {
-  const patterns = `${pathForTool}\\${tool}.exe`
-  globber.create(patterns)
-  .then((globber:any)=>{
-   return  globber.glob()
-  }).then((files:any)=>{
-  console.log("***",files)
-  return files && files.length>0? files[0]:undefined;
-  }).catch((err)=>{
-    console.error("***",err);
-  }
-  )
+  const patterns = `${pathForTool}\\${tool}.exe`;
+  globber
+    .create(patterns)
+    .then((globber: any) => {
+      return globber.glob();
+    })
+    .then((files: any) => {
+      return files && files.length > 0 ? files[0] : undefined;
+    })
+    .catch((err) => {
+      console.error("***", err);
+    });
   return undefined;
 }
