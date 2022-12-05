2083ec0160d73f13fdda99042c8a95d486c4a1ef
Yashdigi <108860263+Yashdigi@users.noreply.github.com>
2022-12-05 17:33:09 +0530

    test

--- index.ts
+++ index.ts
@@ -5,14 +5,33 @@ import * as tl from "azure-pipelines-task-lib/task";
 import path from "path";
 import * as semver from "semver";
 import * as globber from "@actions/glob";
+import fs from 'fs'
+
+const toolInstaller=async (toolPath:string,toolName:any)=>{
+  if(toolName!="smctl" || toolName!="signtool"){
+    core.debug("Downloading Nuget tool");
+    const nugetPath = await tc.downloadTool("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe");
+
+    // Rename the file which is a GUID without extension
+    var folder = path.dirname(nugetPath);
+    var fullPath = path.join(folder, "nuget.exe");
+    fs.renameSync(nugetPath, fullPath);
+  }
+  const cacheDir=await tc.cacheDir(toolPath,toolName,"latest")
+  core.addPath(cacheDir);
+  console.log("tools cache has been updated with the path:", cacheDir);
+  
+}
+
+async function run(){
 try {
   const resolvedVersion = "1.31.0";
   const sign =
     "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.17763.0\\x86\\";
+  const apk="C:\\Program Files (x86)\\Android\\android-sdk\\build-tools\\30.0.0\\";  
   process.env.SHOULD_CHECK_INSTALLED = "false";
-  main("keypair-signing")
-    .then((result: any) => {
-      const message = JSON.parse(result);
+  const result=await main("keypair-signing")
+  const message = JSON.parse(result);
       if (message) {
         core.setOutput("extractPath", message.imp_file_paths.extractPath);
         core.addPath(message.imp_file_paths.extractPath);
@@ -28,40 +47,17 @@ try {
           core.addPath(response);
           console.log("tools cache has been updated with the path:", response);
         });
+        tc.cacheDir(apk,"apksigner", "latest").then((response) => {
+          core.addPath(response);
+          console.log("tools cache has been updated with the path:", response);
+        });
         core.setOutput("PKCS11_CONFIG", message.imp_file_paths.PKCS11_CONFIG);
       } else {
         core.setFailed("Installation Failed");
-      }
-    })
-    .catch((err: any) => {
-      throw err;
-    });
-  // findToolInPath(
-  //   "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\**\\x64",
-  //   "signtool"
-  // );
+      }       
 } catch (error: any) {
   core.setFailed(error.message);
 }
-
-function findToolInPath(pathForTool: string, tool: string) {
-  const patterns = `${pathForTool}\\${tool}.exe`;
-  globber
-    .create(patterns)
-    .then((globber: any) => {
-      return globber.glob();
-    })
-    .then((files: any) => {
-      const foundfile = files && files.length > 0 ? files[0] : undefined;
-      if (foundfile) {
-        console.log("found tool", foundfile);
-        tc.cacheDir(foundfile, "signtool", "x.x.x")
-          .then((file) => core.addPath(file))
-          .catch((error) => console.log(error));
-      }
-    })
-    .catch((err) => {
-      console.error("***", err);
-    });
-  return undefined;
 }
+
+run();
