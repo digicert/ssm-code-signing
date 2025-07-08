7f0e5b8743291028919b60442b056ddf0cd88291
Vishal Shanbhag <vishal.shanbhag@digicert.com>
2025-07-08 14:47:39 +0530

    fix(): [DOSTM-7682]: Unzip Issue Fix for the Self-hosted Runners

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -1,8 +1,16 @@
 import * as core from "@actions/core";
+import { cleanup } from "@digicert/ssm-client-tools-installer";
 
 (async () => {
-    console.log('Received cleanup signal. Initiating graceful shutdown...');
-    const tempPath = core.getState("tempPath");
-    console.log(`Cleaning up temporary path:', ${tempPath}`);
+    console.log('Received cleanup signal. Initiating graceful termination...');
+    console.log(`Cleaning up initiated at ${new Date().toISOString()}`);
+    cleanup(true)
+        .then(() => {
+            console.log('Cleanup completed successfully.');
+        })
+        .catch((error) => {
+            console.error('Error during cleanup:', error);
+            core.setFailed(`Cleanup failed: ${error.message}`);
+        });
     console.log('Cleanup completed. Exiting now.');
 })();
