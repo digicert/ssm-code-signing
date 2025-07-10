09eed93cec107e2649a08c3a0d479cd5be86381f
Vishal Shanbhag <vishal.shanbhag@digicert.com>
2025-07-10 22:07:05 +0530

    fix(): [DOSTM-7682]: validating changes for Self-Hosted runner fix

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -1,16 +1,7 @@
 import * as core from "@actions/core";
-import { cleanup } from "@digicert/ssm-client-tools-installer";
 
 (async () => {
     console.log('Received cleanup signal. Initiating graceful termination...');
     console.log(`Cleaning up initiated at ${new Date().toISOString()}`);
-    cleanup(true)
-        .then(() => {
-            console.log('Cleanup completed successfully.');
-        })
-        .catch((error) => {
-            console.error('Error during cleanup:', error);
-            core.setFailed(`Cleanup failed: ${error.message}`);
-        });
     console.log('Cleanup completed. Exiting now.');
 })();
